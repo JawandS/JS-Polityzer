d9d0890f53fe1383c636e17fcc0520f4dfe3bed0
JawandS <jawandsingh@gmail.com>
2023-11-04 21:09:57 -0400

    update experiment

---
+++ experiment.sh
@@ -0,0 +1,18 @@
+# iteratee four times
+for counter in 1 2 3 4 5
+do
+    # clear previous results
+    ./clear.sh
+    # run the website downloader
+    python3 auto_downloader.py
+    # make a new folder for the testing
+    mkdir ./testing/Run$counter
+    # copy missing_website, downloads from database
+    cp database/missing_website.csv ./testing/Run$counter
+    cp database/downloaded_javascript_links.csv ./testing/Run$counter
+    cp database/downloaded_websites.csv ./testing/Run$counter
+    # move contents of logs dir to the testing folder
+    mv ./logs/* ./testing/Run$counter/logs
+    # copy html to the testing folder
+    cp -r ./html ./testing/Run$counter
+done

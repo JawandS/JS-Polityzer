c18f1f09006b086c1d4a022371e7767beacb96df
JawandS <jawandsingh@gmail.com>
2023-11-05 00:16:52 -0400

    prepare for full experiment

--- experiment.sh
+++ experiment.sh
@@ -1,5 +1,5 @@
 # remove testing directory
-rm -r ./testing
+rm -r ./testing_full
 # iteratee four times
 for counter in 1 2 3 4 5
 do
@@ -8,14 +8,14 @@ do
     # run the website downloader
     python3 auto_downloader.py
     # make a new folder for the testing
-    mkdir -p ./testing/Run$counter
+    mkdir -p ./testing_full/Run$counter
     # copy missing_website, downloads from database
-    cp database/missing_website.csv ./testing/Run$counter
-    cp database/downloaded_javascript_links.csv ./testing/Run$counter
-    cp database/downloaded_websites.csv ./testing/Run$counter
+    cp database/missing_website.csv ./testing_full/Run$counter
+    cp database/downloaded_javascript_links.csv ./testing_full/Run$counter
+    cp database/downloaded_websites.csv ./testing_full/Run$counter
     # move contents of logs dir to the testing folder
-    mkdir -p ./testing/Run$counter/logs
-    mv ./logs/* ./testing/Run$counter/logs
+    mkdir -p ./testing_full/Run$counter/logs
+    mv ./logs/* ./testing_full/Run$counter/logs
     # copy html to the testing folder
-    cp -r ./html ./testing/Run$counter
+    cp -r ./html ./testing_full/Run$counter
 done

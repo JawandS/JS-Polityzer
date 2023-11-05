bb06567a19bedad05bb7956617fb9f2655b91f7a
JawandS <jawandsingh@gmail.com>
2023-11-04 23:05:39 -0400

    clean run

--- experiment.sh
+++ experiment.sh
@@ -6,13 +6,13 @@ do
     # run the website downloader
     python3 auto_downloader.py
     # make a new folder for the testing
-    mkdir ./testing/Run$counter
+    mkdir -p ./testing/Run$counter
     # copy missing_website, downloads from database
     cp database/missing_website.csv ./testing/Run$counter
     cp database/downloaded_javascript_links.csv ./testing/Run$counter
     cp database/downloaded_websites.csv ./testing/Run$counter
     # move contents of logs dir to the testing folder
-    mkdir ./testing/Run$counter/logs
+    mkdir -p ./testing/Run$counter/logs
     mv ./logs/* ./testing/Run$counter/logs
     # copy html to the testing folder
     cp -r ./html ./testing/Run$counter

5949308603e11a1596714a349cfb52f68e16dc30
JawandS <JawandSingh@gmail.com>
2023-11-22 10:04:29 -0500

    failed download and add metadata to active html

--- experiment.sh
+++ experiment.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-test_dir="./testing_full/11_8_2023"
+test_dir="./testing_full/11_21_23"
 # test_dir="./small_test/11_6_23"
 # remove testing directory
 rm -r $test_dir
 # iterate
-for counter in 1 2
+for counter in 1
 do
     # get the directory for current run
     curr_dir=$test_dir/Run$counter

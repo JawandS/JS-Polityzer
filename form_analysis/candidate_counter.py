cfe9eb7fd7dc0c84fddbf2734dde164bb33254cc
JawandS <JawandSingh@gmail.com>
2024-01-02 17:33:23 -0500

    working on candidate form counts

---
+++ form_analysis/candidate_counter.py
@@ -0,0 +1,1 @@
+# count the number of house and senate candidates that have at least one form field not in ignore/misc
--- form_analysis/keyword_classifier.py
+++ form_analysis/keyword_classifier.py
@@ -41,8 +41,8 @@ with open('keyword_mapping.json') as f:
 
 # write the keyword counts sorted by highest to lowest to file
 with open('keyword_count.csv', 'w') as f:
-    house_candidates = 136
-    senate_candidates = 61
+    house_candidates = 90
+    senate_candidates = 47
     f.write('keyword, house_count, house_percent, senate_count, senate_percent\n')
     for keyword in sorted(keyword_count, key=lambda k: keyword_count[k][0] + keyword_count[k][1], reverse=True):
         house_count = keyword_count[keyword][0]

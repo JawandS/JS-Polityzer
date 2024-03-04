216f304c8133a9e88e76a72378bd9959f609f837
JawandS <JawandSingh@gmail.com>
2024-03-03 23:07:37 -0500

    update clusters

--- privacy_policy_analysis/similarity_analysis.py
+++ privacy_policy_analysis/similarity_analysis.py
@@ -18,27 +18,43 @@ vectors = vectorizer.fit_transform(policies.values()).toarray().tolist()
 # dictionary mapping names to vectors
 policy_vectors = {policy_name: vector for policy_name, vector in zip(policies.keys(), vectors)}
 
-# calculate the similarity index
-from sklearn.cluster import KMeans
-num_clusters = 6
-kmeans = KMeans(n_clusters=num_clusters)
-
 # get the vectors value
 vector_values = list(policy_vectors.values())
 
-# fit the kmeans model
-kmeans.fit(vector_values)
+from sklearn.metrics import silhouette_score
+from sklearn.cluster import KMeans
 
-# predict the clusters
-cluster_centers = kmeans.predict(vector_values)
+def k_means(num_clusters, update_file=False):
+    # calculate the similarity index
+    kmeans = KMeans(n_clusters=num_clusters)
 
-# Group vectors by cluster
-clusters = {i: [] for i in range(num_clusters)}
-for i, cluster in enumerate(cluster_centers):
-    clusters[cluster].append(list(policy_vectors.keys())[i])
+    # fit the kmeans model
+    kmeans.fit(vector_values)
 
-# Write each cluster to a separate file
-for cluster, policy_names_in_cluster in clusters.items():
-    with open(f'./clusters/cluster_{cluster}.txt', "w") as f:
-        for policy_name in policy_names_in_cluster:
-            f.write(f"{policy_name}\n")
+    # predict the clusters
+    cluster_centers = kmeans.predict(vector_values)
+
+    if update_file:
+        # Group vectors by cluster
+        clusters = {i: [] for i in range(num_clusters)}
+        for i, cluster in enumerate(cluster_centers):
+            clusters[cluster].append(list(policy_vectors.keys())[i])
+
+        # Write each cluster to a separate file
+        for cluster, policy_names_in_cluster in clusters.items():
+            with open(f'./clusters/cluster_{cluster}.txt', "w") as f:
+                for policy_name in policy_names_in_cluster:
+                    f.write(f"{policy_name}\n")
+
+    # calculate score
+    score = silhouette_score(vector_values, cluster_centers)
+    return score
+
+# calculate the silhouette score for different number of clusters
+# scores = []
+# for i in range(2, 11):
+#     score = k_means(i)
+#     scores.append(score)
+#     print(f"Silhouette score for {i} clusters: {score}")
+
+k_means(num_clusters=8, update_file=True) 
